a56c5455fb08e014a4e7337cb66029be53ce6557
J_D_Nye <garwoodjdm@gmail.com>
2023-10-19 01:43:24 +1100

    server side finished ish (draft)

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,15 +1,38 @@
 const typeDefs = `
 type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
     savedBooks: [Book]!
 }
 
 type Book {
-    bookId: ID
-    authors
+    bookId: ID!
+    authors: [String]!
+    description: String!
+    title: String!
+    image: String!
+    link: String!
 }
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(authors: [String]!, description: String!, bookId: ID!, image: String!, link: String!, title: String!): Book
+    removeBook(bookId: ID!): Book
+  }
+
 `
+
+module.exports = typeDefs
 //More to go here
